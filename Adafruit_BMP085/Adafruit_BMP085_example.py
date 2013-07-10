a5628ba9c6bc2449e71808f488eb20e62415705d
michael tesch <tesch1@gmail.com>
2013-07-09 22:49:14 -0500

    also altitude gets units

--- Adafruit_BMP085/Adafruit_BMP085_example.py
+++ Adafruit_BMP085/Adafruit_BMP085_example.py
@@ -22,4 +22,4 @@ altitude = bmp.readAltitude()
 
 print "Temperature: %.2f C" % temp
 print "Pressure:    %.2f hPa" % (pressure / 100.0)
-print "Altitude:    %.2f" % altitude
+print "Altitude:    %.2f m" % altitude
