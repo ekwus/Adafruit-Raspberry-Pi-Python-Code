c27a9fe35ff800a3faae5e3be59a7963cba8bfe3
michael tesch <tesch1@gmail.com>
2013-07-21 09:22:29 -0500

    added unit conversion for US to BMP085 example

--- Adafruit_BMP085/Adafruit_BMP085_example.py
+++ Adafruit_BMP085/Adafruit_BMP085_example.py
@@ -2,6 +2,18 @@
 
 from Adafruit_BMP085 import BMP085
 
+def C_to_F(C):
+  return C * (180.0 / 100.0) + 32.0
+
+def m_to_ft(m):
+  return 3.2808399 * m
+
+def hPa_to_inHg(hPa):
+  return hPa / 33.86389
+
+def hPa_to_psi(hPa):
+  return hPa / 0.0145037
+
 # ===========================================================================
 # Example Code
 # ===========================================================================
@@ -20,6 +32,6 @@ temp = bmp.readTemperature()
 pressure = bmp.readPressure()
 altitude = bmp.readAltitude()
 
-print "Temperature: %.2f C" % temp
-print "Pressure:    %.2f hPa" % (pressure / 100.0)
-print "Altitude:    %.2f m" % altitude
+print "Temperature: %.2f C / %.2f F" % (temp, C_to_F(temp))
+print "Pressure:    %.2f hPa / %.2f inHg" % ((pressure / 100.0), hPa_to_inHg(pressure))
+print "Altitude:    %.2f m / %.1 ft" % (altitude, m_to_ft(altitude))
